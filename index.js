80bce18d7b9aeda7609ad5d0ce687c61b193c703
Kamlakar Yadav <kamlakar.y@logicaldna.com>
2021-04-08 09:49:14 +0530

    get request added
    Signed-off-by: Kamlakar Yadav <kamlakar.y@logicaldna.com>

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+const cors = require('cors');
 const axios = require("axios");
 const PORT = 3100;
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(
-	bodyParser.urlencoded({
-		extended: true
-	})
-); // for parsing application/x-www-form-urlencoded
+app.use(cors());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({extended: true, limit: '10mb'}));
+
+// Express 3.0
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({extended: true, limit: '10mb'}));
+
+const env = "development";
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json()); 
+
+app.all('/*', function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    if (request.method === 'OPTIONS') {
+        response.status(200).end();
+    } else {
+        next();
+    }
+});
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to TELEGRAM API."});
+});
 
 //This is the route the API will call
 app.post("/new-message", function(req, res) {
-	const { message } = req.body.message;
+    console.log(req);
+	const message = req.body;
 
 	//Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
@@ -49,3 +75,5 @@ app.post("/new-message", function(req, res) {
 app.listen(PORT, function() {
 	console.log(`Telegram app listening on port ${PORT}!`);
 });
+
+module.exports = app;
